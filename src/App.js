5839d039846c117543f5408f76ea29d59739b194
Leslie Villatoro <levill98@gmail.com>
2020-11-11 13:28:58 -0800

    set initial imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import React, { Component } from 'react';
+import movies from './movies.json';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CardList from './components/CardList';
 
-
+//set state to 0
+//onclcick for images
 
 
